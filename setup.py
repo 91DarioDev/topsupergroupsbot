11bc648abecab119f330ba878812e4523589eebb
91DarioDev <dariomsn@hotmail.it>
2017-10-30 17:42:31 +0100

    package

---
+++ setup.py
@@ -0,0 +1,55 @@
+# TopSupergroupsBot - A telegram bot for telegram public groups leaderboards
+# Copyright (C) 2017  Dario <dariomsn@hotmail.it> (github.com/91DarioDev)
+#
+# TopSupergroupsBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# TopSupergroupsBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with TopSupergroupsBot.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import setuptools
+
+
+setuptools.setup(
+
+    name="topsupergroupsbot",
+    version="1",
+
+    license="GPL-3",
+
+    author="Dario 91DarioDev",
+    author_email="dariomsn@hotmail.it",
+
+    install_requires=[
+		"redis",
+		"Babel",
+		"python-telegram-bot",
+		"psycopg2"
+    ],
+
+    packages=[
+        "topsupergroupsbot",
+    ],
+
+    entry_points={
+        "console_scripts": [
+            "topsupergroupsbot = topsupergroupsbot.__main__:main",
+        ],
+    },
+
+    include_package_data=True,
+    zip_safe=False,
+
+    classifiers=[
+        "Not on PyPI"
+    ],
+
+)

597285020492a9ab3ccf7947546811a41cfc43e0
91DarioDev <dariomsn@hotmail.it>
2018-01-26 17:40:23 +0100

    fix

--- topsupergroupsbot/leaderboards.py
+++ topsupergroupsbot/leaderboards.py
@@ -450,7 +450,7 @@ class GroupLeaderboard(Leaderboard):
             else:  # it's a private chat
                 text += "{}) {}: {}\n".format(
                     user[5], 
-                    html.escape("@"+str(user[4]) if user[4] is not None else user[2]), 
+                    escape_markdown("@"+str(user[4]) if user[4] is not None else user[2]), 
                     utils.sep_l(user[1], self.lang)
                 )
         print(text)
